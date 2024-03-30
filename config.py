9e0f80e1c26d2570010858efa2cf9bd004f1f29e
DevTechBytes <devvy@DevTechBytes.com>
2024-03-30 12:54:20 -0500

    refactored code

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-import os
-
 class Config:
-    OLLAMA_MODELS = ('llama2:7b', 'llama2:13b', 'mistral','llama2-uncensored:latest')
+    OLLAMA_MODELS = ('llama2:7b', 'llama2:13b', 'mistral', 'llama2-uncensored:latest')
 
-    SYSTEM_PROMPT = rf"""You are a helpful chatbot that has access to multiple 
-                    open-source models such as {OLLAMA_MODELS}.
+    SYSTEM_PROMPT = rf"""You are a helpful chatbot that has access to the following 
+                    open-source models {OLLAMA_MODELS}.
                     You can can answer questions for users on any topic."""
     
