d17037bc2e2bb25235f2b0367e97954bbf63e045
DevTechBytes <devvy@DevTechBytes.com>
2024-03-31 12:46:36 -0500

    updated chatbot code

--- ollama_chatbot.py
+++ ollama_chatbot.py
@@ -1,9 +1,6 @@
 import streamlit as st
 from config import Config
 from helpers.llm_helper import chat, stream_parser
-from dotenv import load_dotenv
-
-load_dotenv()
 
 st.set_page_config(
     page_title="Streamlit Ollama Chatbot",
