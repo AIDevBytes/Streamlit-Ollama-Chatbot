32b2ce608fa5d73416b036fa175d81b13a5d7756
DevTechBytes <devvy@DevTechBytes.com>
2024-03-31 12:42:17 -0500

    updated starter_brach

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
 class Config:
-    OLLAMA_MODELS = ('codellama:7b', 'codellama:13b', 'llama2:7b', 'llama2:13b', 'mistral', 'llama2-uncensored:latest')
-
-    SYSTEM_PROMPT = rf"""You are a helpful chatbot that has access to the following 
-                    open-source models {OLLAMA_MODELS}.
-                    You can can answer questions for users on any topic."""
+    ...
     
