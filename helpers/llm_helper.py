6f211a4272632097f04159679bcbb9cec2c14987
DevTechBytes <devvy@DevTechBytes.com>
2024-03-31 12:43:07 -0500

    updated starter_brach

--- helpers/llm_helper.py
+++ helpers/llm_helper.py
@@ -4,16 +4,8 @@ from config import Config
 system_prompt = Config.SYSTEM_PROMPT
 
 def chat(user_prompt, model):
-    stream = ollama.chat(
-        model=model,
-        messages=[{'role': 'assistant', 'content': system_prompt},
-                  {'role': 'user', 'content': f"Model being used is {model}.{user_prompt}"}],
-        stream=True,
-    )
-
-    return stream
+    ...
 
 # handles stream response back from LLM
 def stream_parser(stream):
-    for chunk in stream:
-        yield chunk['message']['content']
+    ...
