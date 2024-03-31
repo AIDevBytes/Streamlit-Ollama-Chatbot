da628ba9d83e8778bdf12ec1ae429c49248500d2
DevTechBytes <devvy@DevTechBytes.com>
2024-03-31 12:36:35 -0500

    updated requirements.txt

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    OLLAMA_MODELS = ('llama2:7b', 'llama2:13b', 'mistral', 'llama2-uncensored:latest')
+    OLLAMA_MODELS = ('codellama:7b', 'codellama:13b', 'llama2:7b', 'llama2:13b', 'mistral', 'llama2-uncensored:latest')
 
     SYSTEM_PROMPT = rf"""You are a helpful chatbot that has access to the following 
                     open-source models {OLLAMA_MODELS}.
